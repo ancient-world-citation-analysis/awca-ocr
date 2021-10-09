41878702f97f01b9d854bd515346574c203f5d68
Peter Donovan <p.v.donovan4@gmail.com>
2021-10-08 17:03:22 -0700

    Minor style fix.

--- lang/langcodes.py
+++ lang/langcodes.py
@@ -224,7 +224,7 @@ that may appear in BCP-47 to the shortest available non-deprecated
 ISO codes (avoiding 639-2/B).
 """
 DEPRECATED_TO_CURRENT = {
-    'bh': 'bih', # Bihari. There is no acceptable 2-letter code.
+    'bh': 'bih',  # Bihari. There is no acceptable 2-letter code.
     'in': 'id',  # Indonesian
     'iw': 'he',  # Hebrew
     'ji': 'yi',  # Yiddish
