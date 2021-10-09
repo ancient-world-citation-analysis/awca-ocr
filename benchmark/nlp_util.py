22f6710421099a64230e86fdd5de9980027387f3
Peter Donovan <p.v.donovan4@gmail.com>
2021-10-08 20:53:31 -0700

    Attempt to be less sloppy with definitions.

--- benchmark/nlp_util.py
+++ benchmark/nlp_util.py
@@ -7,10 +7,10 @@ from typing import Callable, Dict, Hashable, Iterable, List, Sequence, Tuple, \
 import re
 
 Regex = str
-Tokenizer = Callable[[str], List[str]]
-Canonicalizer = Callable[[str], str]
 Unit = TypeVar('Unit', bound=Hashable)
-Frequencies = Dict[Tuple[Unit], int]
+Tokenizer = Callable[[str], List[Unit]]
+Canonicalizer = Callable[[str], str]
+NGramBag = Dict[Tuple[Unit], int]
 
 
 def get_canonicalizer(
@@ -27,7 +27,7 @@ def get_canonicalizer(
 def n_gram_counter(
     n: int,
     valid: Callable[[Tuple[Unit]], bool]
-) -> Callable[[Sequence[Unit]], Frequencies]:
+) -> Callable[[Sequence[Unit]], NGramBag]:
     """Returns a function that returns the counts of all n-grams in a
     sequence.
     :param n: the number of units in an n-gram
@@ -35,8 +35,8 @@ def n_gram_counter(
         tuple of Units x,
         (valid(x) = (the truth value of (x is an n-gram)))
     """
-    def count(units: Sequence[Unit]) -> Frequencies:
-        ret: Frequencies = dict()
+    def count(units: Sequence[Unit]) -> NGramBag:
+        ret: NGramBag = dict()
         for i in range(len(units) - n + 1):
             key = cast(Tuple[Unit], tuple(units[i:i+n]))
             if valid(key):
@@ -45,23 +45,34 @@ def n_gram_counter(
     return count
 
 
-def multiset_jaccard_similarity(
-    truth: Frequencies,
-    check: Frequencies
-) -> float:
-    """Returns the multiset Jaccard similarity of `check` to `truth`
-    (which is asymmetric due to normalization by `truth`).
+def multiset_jaccard_similarity(a: NGramBag, b: NGramBag) -> float:
+    """Returns the multiset Jaccard similarity of `a` and `b`.
+    Multiple definitions of this metric seem to exist, but here is one
+    version that has desirable properties:
+    * Symmetry between the two operands
+    * Penalty for failing to include a word that is in the other set
+    * Penalty for including a word that the other set fails to include
+    * Image is [0, 1], which aids communication
+    * Consistency with set Jaccard distance, which also aids
+    communication
     """
-    return 1 - sum(
-        abs(truth.get(key, 0) - check.get(key, 0))
-        for key in truth.keys() | check.keys()
-    ) / sum(
-        truth[key] for key in truth
-    )
+    keys = a.keys() | b.keys()
+    try:
+        return sum(
+            min(a.get(key, 0), b.get(key, 0))
+            for key in keys
+        ) / sum(
+            max(a.get(key, 0), b.get(key, 0))
+            for key in keys
+        )
+    except ZeroDivisionError:
+        # If both sets are empty, then they are as similar as
+        # possible, and 1 is the maximum possible similarity.
+        return 1
 
 
 TOKENIZER: Tokenizer = lambda s: [
-    w for w in re.split(r'(?=[^\w�])|(?<=[^\w�])', s) if w and w.strip()
+    w for w in re.split(r'(?<=[^\w�])|(?=[^\w�])', s) if w.strip()
 ]
 CANONICALIZER: Canonicalizer = get_canonicalizer(
     '-', r'\-|\–',  # Hyphens and en dashes are the same
@@ -86,3 +97,9 @@ _IS_NGRAM: Callable[[Tuple[Unit]], bool] = lambda x: '�' not in x and (
 )
 TRIGRAM = n_gram_counter(3, _IS_NGRAM)
 MONOGRAM = n_gram_counter(1, _IS_NGRAM)
+SIMILARITY: Callable[[int], Callable] = lambda n: lambda a, b: (
+    multiset_jaccard_similarity(
+        n_gram_counter(n, _IS_NGRAM)(TOKENIZER(CANONICALIZER(a))),
+        n_gram_counter(n, _IS_NGRAM)(TOKENIZER(CANONICALIZER(b)))
+    )
+)
