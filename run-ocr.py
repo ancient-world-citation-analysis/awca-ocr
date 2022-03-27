5f2b02c2f08f70043c0a618b277a3f175c43bb5a
Peter Donovan <peterdonovan@berkeley.edu>
2022-03-27 16:40:44 -0700

    Add a quick and dirty runner script.

---
+++ run-ocr.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+from tesseract_manager import Text
+
+parser = argparse.ArgumentParser(description="Run ocr on every 10th document, starting with document i.")
+parser.add_argument("i", help="The zero-based index of the first document to be OCR'd.")
+
+args = parser.parse_args()
+
+print(args.i)
+
+with open("./index.txt", "r") as f:
+    pdfs = f.read().split("\n")
+
+processed_pdfs = set()
+
+for processed_pdfs_list in os.listdir():
+    if "processed_pdfs" in processed_pdfs_list:
+        with open(f"./{processed_pdfs_list}", "r") as f:
+            for pdf in f.read().split("\n"):
+                processed_pdfs.add(pdf)
+
+for idx, pdf in enumerate(pdfs):
+    if idx % 10 == int(args.i) and pdf not in processed_pdfs:
+        if not os.path.isfile(pdf):
+            print("WARNING: Failed to process {pdf}")
+            continue
+        Text(pdf, f"../ocr-output/{idx}_{os.path.basename(pdf).split('.')[0]}").save_ocr()
+        with open(f"processed_pdfs_list_{args.i}.txt", "a") as f:
+            f.write(pdf + "\n")
+    
